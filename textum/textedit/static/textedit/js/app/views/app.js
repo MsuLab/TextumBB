850a9ea30d91117d548bc7088b7e508e3cf00dbb
Boris Kopin <kopbob@gmail.com>
2014-01-20 23:41:15 +0400

    [Textedit] fully moved under requirejs.

---
+++ textum/textedit/static/textedit/js/app/views/app.js
@@ -0,0 +1,12 @@
+
+define(['backbone'], function(Backbone) {
+
+  var App = Backbone.View.extend({
+    initialize: function() {
+      console.log( 'Wahoo!' );
+    }
+  });
+
+  return App;
+  
+});
--- textum/textedit/static/textedit/js/main.js
+++ textum/textedit/static/textedit/js/main.js
@@ -1,11 +1,65 @@
+require.config({
+  'baseUrl': "/static/textedit/js/app",
+  'paths': {
 
-$(function() {
-	$('#some-textarea').wysihtml5();
-	// Uncomment this for scroll enabling
+    // libs
+    'jquery': '../bower_components/jquery/jquery.min',
+    'underscore': '../bower_components/underscore-amd/underscore-min',
+    'backbone': '../bower_components/backbone-amd/backbone-min',
+
+    // jScrollPane
+    'mousewheel': '../bower_components/jquery-mousewheel/jquery.mousewheel',
+    'jScrollPane': '../bower_components/jscrollpane/script/jquery.jscrollpane',
+
+    // bootstrap3-wysihtml5
+    'bootstrap': '../bower_components/bootstrap/dist/js/bootstrap',
+    'handlebars': '../bower_components/handlebars/handlebars',
+    'wysihtml5': '../bower_components/wysihtml5/dist/wysihtml5-0.3.0.min',
+    'editor': '../bower_components/bootstrap3-wysihtml5-bower/dist/bootstrap3-wysihtml5.min'
+  },
+  // Shim declaration
+  'shim': {
+
+    // jScrollPane
+    'jScrollPane': {
+        'deps': ['jquery', 'mousewheel'],
+        'exports': 'jScrollPane'
+    },
+
+    // bootstrap3-wysihtml5
+    'handlebars': {
+      'exports': 'Handlebars'
+    },
+    'bootstrap': {
+        'deps': ['jquery'],
+        'exports': 'bootstrap'
+    },
+    'wysihtml5': {
+        'exports': 'wysihtml5'
+    },
+    'editor': {
+        'deps': ['wysihtml5', 'handlebars', 'jquery', 'bootstrap'],
+        'exports': 'editor'
+    }
+  }
+});
+
+
+require(['views/app', 
+    'jScrollPane', 'bootstrap', 'editor'], // not objects
+function(AppView) {
+
+    $('#some-textarea').wysihtml5();
+
 	$('.photoGallery').jScrollPane(
 		{
             autoReinitialise: true,
             hideFocus: true
         }
     );
+
+  new AppView;
 });
+
+
+
