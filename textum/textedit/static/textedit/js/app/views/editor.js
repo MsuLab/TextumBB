16f425cfbb6dab7b9aaf8c9d477c16ab19ec195f
Vladimir-Kozlov <kozlov2.volodia2@gmail.com>
2014-02-09 23:50:14 +0400

    Started adding AJAX file uploading mechanism. Found it on StackOverflow.

--- textum/textedit/static/textedit/js/app/views/editor.js
+++ textum/textedit/static/textedit/js/app/views/editor.js
@@ -22,10 +22,38 @@ define(['backbone',
         },
         uploadFile: function() {
         		console.log("Upload Button");
+        		
+        		$('#upload').submit(function(){ //!!!!!!!!!!!!!!!!!!!!!!!!
+        			var formData = new FormData($('#upload')[0]);
+               $.ajax({
+        				url: $('#upload').attr('action'),  //Server script to process data
+                  type: 'POST',
+        				xhr: function() {  // Custom XMLHttpRequest
+         		   	var myXhr = $.ajaxSettings.xhr();
+            			/*if(myXhr.upload){ // Check if upload property exists
+            	   		myXhr.upload.addEventListener('progress',progressHandlingFunction, false); // For handling the progress of the upload
+            			}*/
+            		return myXhr;
+        			},
+        			//Ajax events
+        			//beforeSend: beforeSendHandler,
+        			//success: completeHandler,
+        			//error: errorHandler,
+        			// Form data
+        			data: formData,
+        			//Options to tell jQuery not to process data or worry about content-type.
+        			cache: false,
+        			contentType: false,
+        			processData: false
+    				});
+        			//$.post(this.$upload_form.attr('action'), this.$upload_form.serialize());
+        		})
+        		
         		this.$select_file.change(function() {
         			$('#upload').submit();
         		});
         		this.$select_file.click();
+        		//this.$upload_form.submit();
         }
     });
 
