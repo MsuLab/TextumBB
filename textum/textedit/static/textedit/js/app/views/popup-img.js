b233d3f3bd22b93320fc8fd0cba9386425669607
Vladimir-Kozlov <kozlov2.volodia2@gmail.com>
2014-03-01 23:38:54 +0400

    Finally figured out how adding works! I'm making this commit just in case I have to erase everything I've done and start from scratch.

--- textum/textedit/static/textedit/js/app/views/popup-img.js
+++ textum/textedit/static/textedit/js/app/views/popup-img.js
@@ -17,7 +17,7 @@ define(['backbone',
         },
 
         closePopup: function() {
-            $('#uploadImg ul li').remove();
+            $('#uploadImg ul').empty();
             $('#opaco').toggleClass('hidden').removeAttr('style').unbind('click');
             $('#popup').toggleClass('hidden');
             $('#uploadImg').fileupload('destroy');
@@ -51,25 +51,29 @@ define(['backbone',
                 .alignCenter();
 
             self.initImgupload();
+
         },
 
         initImgupload: function () {
             console.log('Image pop-up!');
             var self = this;
+            var index = 0;
             $('#uploadImg').fileupload({
                 dataType: 'json',
-                context: $('#uploadImg')[0],
+                context: $('#uploadImg'),
 
                 add: function(e, data) {
                     //var jqXHR;
                     console.log("File added.");
+                    data.files[0].uploadID = "uploadID" + index;
+                    index = index + 1;
                     data.context = $('<li><span class="btn btn-success fileinput-button uploadImg-uploadThis">\
                                      <span>' + data.files[0].name + '</span>\
                                     </span></li>')
                         //.text('Upload ' + data.files[0].name)
                         .appendTo($('#uploadImg ul'))
                         .click(function() {
-                            data.context = $('<p/>').text('Загружается...').replaceAll($(this));
+                            data.context = $('<li><p>Загружается...</p></li>').replaceAll($(this));//.text('Загружается...').replaceAll($(this));
                             var jqXHR = data.submit();
                             jqXHR.error(function(jqXHR, textStatus, errorThrown) {
                                 if (errorThrown === 'abort') {
@@ -83,10 +87,19 @@ define(['backbone',
                                 //Backbone.trigger('uploadTextFile', result.files[0]);
                                 data.context = $('<p/>').text('Загрузка завершена.').replaceAll($(this));
                             });
+                                            /*setTimeout(function () {
+                                            var overallProgress = $(data.context).fileupload('progress');
+                                            console.log(overallProgress.loaded);}, 100);*/
                         });
                         //jqXHR.success(function(result, textStatus, jqXHR) {
                             //Backbone.trigger('uploadTextFile', result.files[0]);
                         //});
+                },
+
+                progress: function(e, data) {
+                    var progress = parseInt(data.loaded / data.total * 100, 10);
+                    console.log(progress);
+                    console.log(data.files[0].uploadID);
                 },
 
                 done: function(e, data) {
