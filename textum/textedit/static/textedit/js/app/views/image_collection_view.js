b8a97f8a75b26f21a24753f4e1e7286933f70af4
Vladimir-Kozlov <kozlov2.volodia2@gmail.com>
2014-03-24 00:40:51 +0000

    Finished full view with manual selection.

--- textum/textedit/static/textedit/js/app/views/image_collection_view.js
+++ textum/textedit/static/textedit/js/app/views/image_collection_view.js
@@ -3,7 +3,9 @@ define(['underscore',
     'models/timage',
     'collections/timages',
     'views/image_view',
-], function (_, Backbone, TImage, TImages, TImageView) {
+    'text!templates/gallery_menu_normal_view.html',
+    'text!templates/gallery_menu_full_view.html'
+], function (_, Backbone, TImage, TImages, TImageView, Normal, Full) {
 	var imageCollection = Backbone.View.extend({
 		el: '.photoGrid',
 
@@ -13,26 +15,19 @@ define(['underscore',
 				reset: true,
 			});
 			this.render();
-
+            $('.galleryMenu').empty().append(Normal);
             this.collection.on("add", this.renderImage, this);
             this.collection.on("reset", this.render, this);
-            this.collection.on("change", this.change, this);
             var self = this;
             this.selected = undefined;
             this.selectedModel = undefined;
-            $('html').click(function (){
-                $('#' + self.selected).removeClass('image-selected');
+            $('body').click(function (){
+                $(self.selected).removeClass('image-selected');
                 self.selected = undefined;
                 self.selectedModel = undefined;
             });
             $('.full-view-switch').click(function () {
-                Backbone.trigger('full-view');
-                $('#' + self.selected).removeClass('image-selected');
-                self.selected = undefined;
-                self.$el.empty();
-                self.renderImage(self.collection.get(self.selectedModel));
-                self.selectedModel = undefined;
-                $('.image').addClass('fullImage').removeClass('image');
+                self.switch2Full();
             });
 		},
 
@@ -42,15 +37,6 @@ define(['underscore',
             this.collection.each(function (image) {
             	self.renderImage(image);
             });
-            $('.image').click(function (event) {
-                $('#' + self.selected).removeClass('image-selected');
-                self.selected = this.id;
-                var tmp = this.id;
-                self.selectedModel = tmp.replace('image', '');
-                //console.log(self.selectedModel);
-                $('#' + self.selected).addClass('image-selected');
-                event.stopPropagation();
-            });
         },
 
         renderImage: function (image) {
@@ -58,43 +44,52 @@ define(['underscore',
                 model: image,
             });
             this.$el.append(imageView.render().el);
-            var inputForm = $('#pageNumInput' + image.id);
-            inputForm.submit(function (e) {
-                var inputField = $(inputForm).find('input');
-                var page_num = $(inputField).val();
-                var n = ~~Number(page_num);
-                if (String(n) === page_num && n > 0) {
-                    image.save('page_num', n);
-                    $(inputField).val('').blur();
-                }
-                else {
-                    $(inputField).val('');
-                }
-                return false;
-            }); /* Something that looks very promising */
-
-        },
-
-        change: function () {
             var self = this;
-            this.collection.each(function (image) {
-                var inputForm = $('#pageNumInput' + image.id);
-                $(inputForm).off();
-                $(inputForm).submit(function (e) {
-                    var inputField = $(inputForm).find('input');
-                    var page_num = $(inputField).val();
-                    var n = ~~Number(page_num);
-                    if (String(n) === page_num && n > 0) {
-                        image.save('page_num', n);
-                        $(inputField).val('').blur();
-                    }
-                    else {
-                        $(inputField).val('');
-                    }
-                    return false;
-                });
+            $('#image' + image.id).off('click');
+            $('#image' + image.id).click(function (event) {
+                $(self.selected).removeClass('image-selected');
+                self.selected = '#image' + image.id;
+                self.selectedModel = image.id;
+                $(self.selected).addClass('image-selected');
+                event.stopPropagation();
+            });
+            $('#image' + image.id).dblclick(function () {
+                self.switch2Full();
             });
         },
+
+        switch2Full: function() {
+            var self = this;
+            if (self.selectedModel != undefined) {
+                Backbone.trigger('full-view');
+                $(self.selected).removeClass('image-selected');
+                self.selected = undefined;
+                self.$el.empty();
+                self.renderImage(self.collection.get(self.selectedModel));
+                self.selectedModel = undefined;
+                $('.image').addClass('fullImage').removeClass('image').off('click');
+                $('.galleryMenu').html(Full);
+                $('.full-view-switch').off('click');
+                $('.full-view-switch').click(function () {
+                    self.switch2Normal();
+                });
+            }
+        },
+
+        switch2Normal: function() {
+            Backbone.trigger('full-view'); //in order to remove that one view for full view
+            $(this.selected).removeClass('image-selected');
+            this.selected = undefined;
+            this.selectedModel = undefined;
+            this.render();
+            $('.galleryMenu').html(Normal);
+            $('.full-view-switch').off('click');
+            var self = this;
+            $('.full-view-switch').click(function () {
+                self.switch2Full();
+            });
+        },
+
 	});
 	return imageCollection;
 });
--- textum/textedit/static/textedit/js/app/views/image_view.js
+++ textum/textedit/static/textedit/js/app/views/image_view.js
@@ -1,7 +1,7 @@
 define(['backbone',
 	'models/timage',
 	'text!templates/imgview_image_template.html',
-], function(Backbone, TImage, Template) {
+], function (Backbone, TImage, Template) {
 	var TImageView = Backbone.View.extend({
 		tagName: 'li',
 		className: 'image',
@@ -13,6 +13,22 @@ define(['backbone',
 		},
 		render: function () {
 			this.$el.html(this.template(this.model.attributes)).attr('id', 'image' + this.model.id);
+            var inputForm = this.$el.find('form');
+            inputForm.off();
+            var self = this;
+            inputForm.submit(function (e) {
+                var inputField = $(inputForm).find('input');
+                var page_num = $(inputField).val();
+                var n = ~~Number(page_num);
+                if (String(n) === page_num && n > 0) {
+                    self.model.save('page_num', n);
+                    $(inputField).val('').blur();
+                }
+                else {
+                    $(inputField).val('');
+                }
+                return false;
+            });
 			return this;
 		},
 		deleteImg: function () {
