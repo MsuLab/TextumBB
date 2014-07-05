98e7c321d3a3abfc0eeaa5b2d7451c3b753fbeb7
Boris Kopin <kopbob@gmail.com>
2014-07-05 14:16:19 +0400

    Add local settings support.

--- textum/settings.py
+++ textum/settings.py
@@ -57,7 +57,6 @@ USE_TZ = True
 # Example: "/var/www/example.com/media/"
 
 MEDIA_ROOT = os.path.join(os.path.dirname(__file__), '..', 'media')
-print "MEDIA_ROOT", MEDIA_ROOT
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://example.com/media/", "http://media.example.com/"
@@ -172,3 +171,8 @@ LOGGING = {
 }
 
 TASTYPIE_ALLOW_MISSING_SLASH = True
+
+try:
+    from .local_dev import *
+except ImportError:
+    pass
